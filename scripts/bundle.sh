6b8994c446748637d98ab2666347d566e770bcdc
Giovanni Vigna <giovanni.vigna@gmail.com>
2026-03-16 15:22:29 -0700

    tweak

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -7,7 +7,7 @@ echo "ictf{ThisIsaFakeFlagFor${CHALLENGE_NAME}}" > ${CHALLENGE_NAME}-bundle/flag
 mkdir ${CHALLENGE_NAME}-bundle/ro
 mkdir ${CHALLENGE_NAME}-bundle/rw
 cp -R ./src ${CHALLENGE_NAME}-bundle/
-cp -R ./scripts ${CHALLENGE_NAME}-bundle/
+cp -R -L ./scripts ${CHALLENGE_NAME}-bundle/
 cp Dockerfile ${CHALLENGE_NAME}-bundle/
 
 tar cvzf ${CHALLENGE_NAME}-bundle.tgz ${CHALLENGE_NAME}-bundle
